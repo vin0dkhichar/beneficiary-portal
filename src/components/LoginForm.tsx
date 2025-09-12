5606b6e1bc40252a42b1cfe327b8413927d426d2
vin0dkhichar <abbb61667@gmail.com>
2025-09-12 11:56:59 +0530

    Add translation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,13 @@
 "use client";
+
 import Image from "next/image";
 import { SyntheticEvent } from "react";
 import { LoginProviders } from "@/components";
+import { useTranslations } from "next-intl";
 
 export default function LoginForm() {
+    const t = useTranslations("loginPage");
+
     function handleSubmit(e: SyntheticEvent) {
         e.preventDefault();
         console.log("Login submitted");
@@ -13,19 +17,19 @@ export default function LoginForm() {
         <div className="w-[450px] bg-[#D9D9D9] rounded-lg shadow-lg flex flex-col items-center p-6">
             <Image
                 src="/login.png"
-                alt="Beneficiary Portal Logo"
+                alt={t("portalName")}
                 width={80}
                 height={80}
                 priority
                 className="mb-4"
             />
 
-            <p className="text-2xl font-semibold text-black mb-6">Beneficiary Portal</p>
+            <p className="text-2xl font-semibold text-black mb-6">{t("portalName")}</p>
 
             <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4 px-8">
                 <div className="flex flex-col">
                     <label className="mb-1 text-md font-medium text-black">
-                        Email or Phone
+                        {t("emailOrPhone")}
                     </label>
                     <input
                         type="text"
@@ -34,7 +38,7 @@ export default function LoginForm() {
                 </div>
 
                 <div className="flex flex-col">
-                    <label className="mb-1 text-md font-medium text-black">Password</label>
+                    <label className="mb-1 text-md font-medium text-black">{t("password")}</label>
                     <input
                         type="password"
                         className="px-3 py-2 text-black bg-white border-gray-400 border rounded-md focus:outline-none"
@@ -44,7 +48,7 @@ export default function LoginForm() {
                             href="/reset-password"
                             className="text-sm text-gray-600 hover:text-black font-bold"
                         >
-                            Reset Password?
+                            {t("resetPassword")}
                         </a>
                     </div>
                 </div>
@@ -53,7 +57,7 @@ export default function LoginForm() {
                     type="submit"
                     className="mt-2 w-full bg-black text-lg text-white py-2 rounded-md font-semibold hover:bg-gray-950 transition cursor-pointer"
                 >
-                    LOGIN
+                    {t("login")}
                 </button>
 
                 <LoginProviders />
@@ -61,3 +65,4 @@ export default function LoginForm() {
         </div>
     );
 }
+
